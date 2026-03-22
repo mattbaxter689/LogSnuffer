990deb884d6a2251e727825cfd8fb861aedaef43
Matthew Baxter <matthewbaxter0412@hotmail.com>
2026-03-22 17:54:50 -0400

    Agent state is updated to now handle new additional items

--- src/state/agent_state.rs
+++ src/state/agent_state.rs
@@ -1,12 +1,19 @@
 use crate::{
     github::issues::IssueMetadata,
-    ticket_tool::analysis_tool::{CriticalError, Warning},
+    ticket_tool::analysis_tool::{AnalysisArgs, CriticalError, Warning},
 };
+use serde::Deserialize;
 
 #[derive(Default, Debug)]
 pub struct AgentState {
     pub closed_issues: Vec<IssueMetadata>,
-    pub summary: String,
-    pub critical_errors: Vec<CriticalError>,
-    pub warnings: Vec<Warning>,
+    pub analysis: Option<AnalysisArgs>,
+    pub processed_warnings: Vec<Warning>,
+    pub processed_errors: Vec<CriticalError>,
+    pub errors_processed: bool,
+    pub warnings_processed: bool,
 }
+
+// This is here to reference for tools. I should have a better structure
+#[derive(Deserialize)]
+pub struct EmptyArgs {}
